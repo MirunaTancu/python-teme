9a971cf3912ff4f081905102bc5598b2bed83ccf
MirunaTancu <152640394+MirunaTancu@users.noreply.github.com>
2023-12-08 20:22:23 +0200

    Add files via upload

---
+++ tema1.py
@@ -0,0 +1,21 @@
+my_list= [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+my_new_list= my_list.copy()
+my_new_list.sort()
+print("Lista data ordonata crescator: ")
+print(my_new_list)
+my_new_list.reverse()
+print("Lista data ordonata descrescator: ")
+print(my_new_list)
+my_new_list.reverse()
+print("Numerele pare din lista : ")
+print(my_new_list[1::2])
+print("Numerele impare din lista : ")
+print(my_new_list[::2])
+print("Multiplii de 3 ai listei sunt : ")
+print(my_new_list[2::3])
+
+
+
+
+
+
